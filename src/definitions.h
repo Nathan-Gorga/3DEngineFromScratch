14a991bf9897eefe059db6e8dc8ce54ea3853cd6
Nathan Gorga <gorga.nathan05@gmail.com>
2025-06-23 21:18:40 +0200

    initRenderer

---
+++ src/definitions.h
@@ -0,0 +1,8 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
+#include "definitions.h"
+
+typedef struct float3{
+    float x; 
+    float y; 
+    float z;
+} float3;
+
+typedef struct float2{
+    float x; 
+    float y;
+}float2;
+
+
+SDL_Renderer * initRenderer(SDL_Window *window){
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Set draw color to black and clear the screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Set draw color to white
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    
+    return renderer;
+}
 
 int main(void) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -14,14 +38,8 @@ int main(void) {
         SDL_WINDOW_SHOWN
     );
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = initRenderer(window);
 
-    // Set draw color to black and clear the screen
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    // Set draw color to white
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     // Draw a simple 2D line
     SDL_RenderDrawLine(renderer, 100, 100, 500, 400);
 
