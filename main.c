38114074afb102e258c9be697e4b85b4b6aecf6c
Nathan Gorga <gorga.nathan05@gmail.com>
2025-06-23 20:57:34 +0200

    print a 2D line on screen

--- main.c
+++ main.c
@@ -1,5 +1,38 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main(void){
+int main(void) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "2D Line Example",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Set draw color to black and clear the screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Set draw color to white
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // Draw a simple 2D line
+    SDL_RenderDrawLine(renderer, 100, 100, 500, 400);
+
+    // Present to the screen
+    SDL_RenderPresent(renderer);
+
+    // Wait a bit so we can see the result
+    SDL_Delay(3000); // 3 seconds
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
